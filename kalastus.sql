9583a8906d32ec0ba35155e29a57616038cfbfa0
SpireTM <48876949+SpireTM@users.noreply.github.com>
2025-08-12 13:51:16 +0300

    Update kalastus.sql

--- kalastus.sql
+++ kalastus.sql
@@ -3,7 +3,6 @@ INSERT INTO `items` (name, label, weight) VALUES
 	('fishingrod', 'Fishing rod', 2),
 	('lbait', 'Fishing bait', 100),
 	('lEbait', 'Extreme Fishing Bait', 100),
-	('fixkit', 'Kit réparation', 3),
-	('carokit', 'Kit carosserie', 3)
 ;
+
 
