446bcb02743a032a233801931737ef24a232bde9
SpireTM <48876949+SpireTM@users.noreply.github.com>
2025-08-12 13:50:47 +0300

    Update kalastus.sql
    I replaced the old ESX inventory limit system with the new weight-based system.

--- kalastus.sql
+++ kalastus.sql
@@ -1,7 +1,9 @@
-INSERT INTO `items` (name, label, `limit`) VALUES
-	('lfish', 'Fish', -1),
-	('fishingrod', 'Fishing rod', 1),
+INSERT INTO `items` (name, label, weight) VALUES
+	('lfish', 'Fish', 30),
+	('fishingrod', 'Fishing rod', 2),
 	('lbait', 'Fishing bait', 100),
-	('lEbait', 'Extreme Fishing Bait', 100)
-	
+	('lEbait', 'Extreme Fishing Bait', 100),
+	('fixkit', 'Kit réparation', 3),
+	('carokit', 'Kit carosserie', 3)
 ;
+
